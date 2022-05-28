b56a755dfac88ebcde389dd2b9df031ba7d05079
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-28 20:50:13 -0300

    Add files via upload

---
+++ bubble_map.R
@@ -0,0 +1,23 @@
+
+# Bubble map -------------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 28/05/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: The R Graph Galery -----------------------------------------------------------------------------------------------------------
+
+# Conceito ---------------------------------------------------------------------------------------------------------------------------------
+
+### Bubble maps são mapas com marcadores no topo. Esses marcadores podem ser círculos
+### com tamanhos proporcionais aos valores numéricos, resultando é um bubble map.
+### Antes de explorar essa seção, aprenda como construir o background do mapa com R.
+
+# Bubble maps interativos com o pacote leaflet ---------------------------------------------------------------------------------------------
+
+### O pacote leaflet do R é um invólucro da biblioteca javascript.js. Ele permite 
+### construir mapas interativos com R em minutos. Os mapas podem ser exportados em
+### html, ou inseridos em arquivos RMarkdown e shiny. 
+
+# Exemplo de mapa com o pacote leaflet -----------------------------------------------------------------------------------------------------
+
+
+
+
