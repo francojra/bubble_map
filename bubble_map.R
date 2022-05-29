fdb13f5cb9133a070addf9ea867a087275f31e78
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-28 23:40:47 -0300

    Add files via upload

--- bubble_map.R
+++ bubble_map.R
@@ -37,3 +37,32 @@ data_red <- data.frame(LONG = 42 + rnorm(10), LAT = 23 + rnorm(10),
                        PLACE = paste("Red_place_",seq(1,10)))
 data_blue <- data.frame(LONG = 42 + rnorm(10), LAT = 23 + rnorm(10), 
                         PLACE = paste("Blue_place_", seq(1,10)))
+
+# Iniciando o leaflet map ------------------------------------------------------------------------------------------------------------------
+
+m <- leaflet() %>% 
+  setView(lng = 42, lat = 23, zoom = 6 ) %>%
+
+  # Add two tiles/backgrounds
+  addProviderTiles("Esri.WorldImagery", group = "background 1") %>%
+  addTiles(options = providerTileOptions(noWrap = TRUE), group = "background 2") %>%
+
+  # Add 2 marker groups
+  addCircleMarkers(data = data_red, lng = ~ LONG , lat = ~ LAT, radius = 8 , 
+                   color = "black",
+                   fillColor = "red", stroke = TRUE, fillOpacity = 0.8, 
+                   group = "Red") %>%
+  addCircleMarkers(data = data_blue, lng = ~ LONG , lat = ~ LAT, radius = 8 , 
+                   color = "black",
+                   fillColor = "blue", stroke = TRUE, fillOpacity = 0.8, 
+                   group = "Blue") %>%
+
+  # Adicionar o widget de controlo
+  addLayersControl(overlayGroups = c("Red","Blue") , baseGroups = c("background 1","background 2"), 
+                   options = layersControlOptions(collapsed = FALSE))
+
+m
+
+# save the widget in a html file if needed.
+# library(htmlwidgets)
+# saveWidget(m, file=paste0( getwd(), "/HtmlWidget/bubblemapControl.html"))
